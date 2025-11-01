a80174a81607c763be604e86b4528825af61a659
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-11-01 20:59:46 +0100

    feat: env + not found page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@workspace/ui/components/button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-dvh flex flex-col justify-center items-center bg-gradient-to-b from-background to-muted/20">
+      <div className="text-center space-y-6 px-4 max-w-md">
+        <div className="space-y-2">
+          <h1 className="text-9xl font-bold text-primary">404</h1>
+          <h2 className="text-3xl font-semibold tracking-tight">
+            Page Not Found
+          </h2>
+          <p className="text-muted-foreground text-lg">
+            The page you&apos;re looking for doesn&apos;t exist or has been
+            moved.
+          </p>
+        </div>
+        <Button
+          size={"lg"}
+          asChild
+          value={"link"}
+          className="h-12 text-xl font-semibold"
+        >
+          <Link href="/">Back to Home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
