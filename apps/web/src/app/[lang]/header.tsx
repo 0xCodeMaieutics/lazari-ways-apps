fbe3794c007ed24046d3038c980e572d663502fc
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-11-05 16:32:15 +0100

    feat: border

--- apps/web/src/app/[lang]/header.tsx
+++ apps/web/src/app/[lang]/header.tsx
@@ -61,61 +61,62 @@ export const Header = ({ translations }: { translations: Translations }) => {
   ];
 
   return (
-    <div className="fixed top-0 left-1/2 -translate-x-1/2 z-50 flex justify-between w-full mx-auto max-w-6xl py-6 px-4 bg-background">
-      {/* <Image src="/logo.png" width={200} height={1081.06} alt="Logo" /> */}
-      <TextLogo>{APP_NAME}</TextLogo>
-
-      <nav className="hidden sm:flex items-center gap-4">
-        {NAV_LINKS.map((link) => (
-          <Button
-            className="relative font-semibold group hover:no-underline text-lg px-0.5 cursor-pointer"
-            variant={"link"}
-            key={link.href}
-            onClick={() => {
-              scrollSmoothlyToSection(link.href);
-            }}
-          >
-            {link.label}
-            <Underline />
-          </Button>
-        ))}
-      </nav>
-      <div className="hidden sm:block">
-        <WhatsappButton />
-        {/* <Image src={"/whatsapp2.png"} width={32} height={32} alt="Whatsapp" /> */}
+    <div className="fixed top-0 left-1/2 -translate-x-1/2 z-50 w-full py-6 px-4 bg-background border-b">
+      <div className="flex justify-between max-w-6xl mx-auto">
+        {/* <Image src="/logo.png" width={200} height={1081.06} alt="Logo" /> */}
+        <TextLogo>{APP_NAME}</TextLogo>
+        <nav className="hidden sm:flex items-center gap-4">
+          {NAV_LINKS.map((link) => (
+            <Button
+              className="relative font-semibold group hover:no-underline text-lg px-0.5 cursor-pointer"
+              variant={"link"}
+              key={link.href}
+              onClick={() => {
+                scrollSmoothlyToSection(link.href);
+              }}
+            >
+              {link.label}
+              <Underline />
+            </Button>
+          ))}
+        </nav>
+        <div className="hidden sm:block">
+          <WhatsappButton />
+          {/* <Image src={"/whatsapp2.png"} width={32} height={32} alt="Whatsapp" /> */}
+        </div>
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <button className="sm:hidden">
+              <Menu size={24} />
+            </button>
+          </SheetTrigger>
+          <SheetContent className="w-full">
+            <SheetHeader>
+              <SheetTitle>{APP_NAME}</SheetTitle>
+              <SheetDescription>{APP_TAGLINE}</SheetDescription>
+            </SheetHeader>
+            <div className="flex flex-col items-center gap-y-3 mt-6">
+              {NAV_LINKS.map((link) => (
+                <Button
+                  className="w-full max-w-max relative font-semibold group hover:no-underline text-lg px-0.5 cursor-pointer"
+                  variant={"link"}
+                  key={link.href}
+                  onClick={() => {
+                    setOpen(false);
+                    sleep(300).then(() => {
+                      scrollSmoothlyToSection(link.href);
+                    });
+                  }}
+                >
+                  {link.label}
+                  <Underline />
+                </Button>
+              ))}
+              <WhatsappButton />
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
-      <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger asChild>
-          <button className="sm:hidden">
-            <Menu size={24} />
-          </button>
-        </SheetTrigger>
-        <SheetContent className="w-full">
-          <SheetHeader>
-            <SheetTitle>{APP_NAME}</SheetTitle>
-            <SheetDescription>{APP_TAGLINE}</SheetDescription>
-          </SheetHeader>
-          <div className="flex flex-col items-center gap-y-3 mt-6">
-            {NAV_LINKS.map((link) => (
-              <Button
-                className="w-full max-w-max relative font-semibold group hover:no-underline text-lg px-0.5 cursor-pointer"
-                variant={"link"}
-                key={link.href}
-                onClick={() => {
-                  setOpen(false);
-                  sleep(300).then(() => {
-                    scrollSmoothlyToSection(link.href);
-                  });
-                }}
-              >
-                {link.label}
-                <Underline />
-              </Button>
-            ))}
-            <WhatsappButton />
-          </div>
-        </SheetContent>
-      </Sheet>
     </div>
   );
 };
