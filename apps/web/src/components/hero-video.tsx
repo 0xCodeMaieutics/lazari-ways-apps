6f70b6529513d807572d6717309b1ffb9dade038
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-11-14 18:23:45 +0100

    asdf

--- apps/web/src/components/hero-video.tsx
+++ apps/web/src/components/hero-video.tsx
@@ -5,7 +5,7 @@ export const HeroVideo = () => {
       muted
       loop
       playsInline
-      className="rounded-2xl w-full bg-red-200 max-w-sm sm:min-w-[350px]"
+      className="rounded-2xl w-full max-w-sm sm:min-w-[350px]"
     >
       <source src="/images/employers/hero.mp4" type="video/mp4" />
       Your browser does not support the video tag.
