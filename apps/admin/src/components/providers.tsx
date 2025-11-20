ab8b598ecbf702e1eb64f019cd3e5d65b33b3185
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-11-20 17:41:54 +0100

    feat(admin): header

--- apps/admin/src/components/providers.tsx
+++ apps/admin/src/components/providers.tsx
@@ -1,12 +1,36 @@
 "use client";
 
 import * as React from "react";
+import Image from "next/image";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "@/lib/get-query-client";
+import Link from "next/link";
+import { Button } from "@workspace/ui/components/button";
+
+const LogoAndText = () => {
+  return (
+    <Link href={"/"}>
+      <div className="relative h-12 w-[190px]">
+        <Image
+          src={"/images/logos/logo-text.svg"}
+          alt="Lazary Ways image Logo"
+          fill
+        />
+      </div>
+    </Link>
+  );
+};
+
+const Underline = () => (
+  <span className="absolute bottom-0 left-0 w-full h-0.5 bg-current opacity-0 group-hover:opacity-100 transition-opacity duration-150 ease-in-out" />
+);
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
+
+  const logoRef = React.useRef<HTMLDivElement>(null);
+
   return (
     <NextThemesProvider
       attribute="class"
@@ -14,7 +38,49 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <header className="fixed top-0 left-1/2 -translate-x-1/2 z-50 w-full pt-4 pb-2 md:py-6 px-4 bg-background border-b transition-transform duration-300 ease-in-out">
+          <div className="flex items-center justify-between max-w-7xl mx-auto">
+            <div className="relative" ref={logoRef}>
+              <LogoAndText />
+              <span className="absolute -bottom-0.5 -right-6 font-semibold text-muted-foreground text-xs">
+                ადმინისტრატორებისთვის
+              </span>
+            </div>
+            <nav className="flex-1 flex justify-center gap-3">
+              <Button
+                className="flex items-center gap-2 relative font-semibold group hover:no-underline text-lg px-0.5 cursor-pointer max-w-max"
+                variant={"link"}
+                asChild
+              >
+                <Link href={""}>
+                  ვაკანსიები
+                  <Underline />
+                </Link>
+              </Button>
+              <Button
+                className="flex items-center gap-2 relative font-semibold group hover:no-underline text-lg px-0.5 cursor-pointer max-w-max"
+                variant={"link"}
+                asChild
+              >
+                <Link href={""}>
+                  კანდიდატები
+                  <Underline />
+                </Link>
+              </Button>
+            </nav>
+            <div
+              style={{
+                width: logoRef.current?.clientWidth,
+              }}
+              className="text-white"
+            >
+              placeholder
+            </div>
+          </div>
+        </header>
+        {children}
+      </QueryClientProvider>
     </NextThemesProvider>
   );
 }
