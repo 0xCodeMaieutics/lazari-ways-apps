9cd85416bf2f5dbf951a1115f70961970d672e30
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-11-29 17:26:15 +0100

    feat(web): added remote patterns config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -14,7 +14,14 @@ const nextConfig = {
   transpilePackages: ["@workspace/ui", "@workspace/shared"],
   images: {
     // domains: ["127.0.0.1", "localhost"], // 👈 Add this line
-    remotePatterns,
+    remotePatterns: [
+      ...remotePatterns,
+      {
+        protocol: "https",
+        hostname: "s3.eu-central-1.amazonaws.com",
+        pathname: "/lazari-ways-bucket/**/*",
+      },
+    ],
   },
 };
 
