6756f7f5ec2a1a042d89e19df4cbfa74059bd24b
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-12-08 19:07:48 +0100

    refactor(file-upload): replaced hard-coded value with env variable

--- packages/file-upload/scripts/upload.ts
+++ packages/file-upload/scripts/upload.ts
@@ -1,9 +1,19 @@
 import { uploadToStorage } from "@workspace/file-upload/s3-client.ts";
+import { zodParse } from "@workspace/shared/error-handling/index.ts";
 import { randomUUID } from "crypto";
-import { read, readFile, readFileSync } from "fs";
-import { env } from "process";
+import { readFileSync } from "fs";
+import z from "zod";
 
 void (async function main() {
+  const s3BucketNameResult = zodParse(
+    process.env.S3_BUCKET_NAME,
+    z.string().min(1)
+  );
+  if (s3BucketNameResult.isErr()) {
+    console.error("S3_BUCKET_ENV_LOAD_FAILED");
+    return;
+  }
+
   const id = randomUUID();
 
   const photoData = readFileSync("./scripts/photo.png");
@@ -12,7 +22,7 @@ void (async function main() {
 
   const uploadResult = await uploadToStorage({
     file: photoData,
-    bucket: "lazari-ways-bucket",
+    bucket: s3BucketNameResult.value,
     fileKey: fileKey,
   });
 
