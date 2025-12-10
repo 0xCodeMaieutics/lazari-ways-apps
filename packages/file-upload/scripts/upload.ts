2cdff0530932c3b4a487e2c2e147de0471633abb
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-12-10 11:42:11 +0100

    feat(upload/scripts/upload.ts): changed script

--- packages/file-upload/scripts/upload.ts
+++ packages/file-upload/scripts/upload.ts
@@ -1,8 +1,13 @@
-import { uploadToStorage } from "@workspace/file-upload/s3-client.ts";
+import {
+  getSignedUrlForDownload,
+  uploadToStorage,
+} from "@workspace/file-upload/s3-client.ts";
+import { keyBuilders } from "@workspace/file-upload/key-builder.ts";
 import { zodParse } from "@workspace/shared/error-handling/index.ts";
 import { randomUUID } from "crypto";
 import { readFileSync } from "fs";
 import z from "zod";
+import { generateRandomString } from "@workspace/shared/lib/random.ts";
 
 void (async function main() {
   const s3BucketNameResult = zodParse(
@@ -17,7 +22,13 @@ void (async function main() {
   const id = randomUUID();
 
   const photoData = readFileSync("./scripts/photo.png");
-  const fileKey = `vacancies/${id}/photo/${Date.now()}-photo.png`;
+
+  const employeeId = generateRandomString(32);
+  const fileKey = keyBuilders.employees.photo.buildKey({
+    employeeId,
+    filename: "photo.png",
+    now: Date.now(),
+  });
   console.log({ fileKey });
 
   const uploadResult = await uploadToStorage({
@@ -34,4 +45,18 @@ void (async function main() {
       errorMessage: "Failed to upload photo",
     };
   }
+
+  const url = await getSignedUrlForDownload({
+    bucket: s3BucketNameResult.value,
+    fileKey,
+    expiresInSeconds: 3600,
+  });
+
+  if (url.isErr()) {
+    console.log("URL_GENERATION_FAILED");
+    return;
+  }
+  console.log({
+    signedUrl: url.value,
+  });
 })();
